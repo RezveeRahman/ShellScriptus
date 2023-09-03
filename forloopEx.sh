a4f16e4f8234fbdb4e56a89a98df3bcd4bbf51fd
Rezvee <52466279+RezveeRahman@users.noreply.github.com>
2023-09-03 12:49:05 -0400

    removed extraneous files (#5)

--- forloopEx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for i in hello 1 * 2 goodbye
-do
-  echo "Looping ... i is set to $i"
-done
--- loginExample.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-read -p "Enter your Username: " usr_name
-read -sp "Enter password: " pass_word
-echo "Your user name is $usr_name"
-echo "Your password is $pass_word"
